b8e9b29f66ac5c6248766300d522d50b8f78ff4d
Bhargav-devops <166719885+Bhargav-devops@users.noreply.github.com>
2025-02-28 07:09:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -128,24 +128,24 @@ resource "aws_key_pair" "minikube_keypair" {
 # EC2 instance
 #####
 
-data "aws_ami" "centos7" {
-  most_recent = true
-  owners = ["aws-marketplace"]
+data "aws_ami" "centos7"{
+    owners = ["973714476881"]
+    most_recent      = true
 
-  filter {
-    name = "product-code"
-    values = ["aw0evgkw8e5c1q413zgy5pjce", "cvugziknvmxgqna9noibqnnsy"]
-  }
+    filter {
+        name   = "name"
+        values = ["Centos-8-DevOps-Practice"]
+    }
 
-  filter {
-    name = "architecture"
-    values = ["x86_64"]
-  }
+    filter {
+        name   = "root-device-type"
+        values = ["ebs"]
+    }
 
-  filter {
-    name = "virtualization-type"
-    values = ["hvm"]
-  }
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
 }
 
 resource "aws_eip" "minikube" {
